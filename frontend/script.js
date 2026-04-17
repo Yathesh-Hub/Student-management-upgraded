737beca6a420dae9b6f74096d9e6ee73de40e105
Yathesh <yatheshinfo@gmail.com>
2026-04-17 10:15:50 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -29,7 +29,7 @@ function togglePassword(id, icon) {
 
 // ── IMPORTANT FIX (NO LOCALHOST) ─────────────────────────────────────────────
 
-const API = ""; 
+const API = "http://ec2-18-139-161-32.ap-southeast-1.compute.amazonaws.com:5000";
 // OR: const API = "http://54.255.237.141:5000";
 
 // ── SIGNUP ───────────────────────────────────────────────────────────────────
